b716a7e657900f9bb26f26e9ae847f2dc0ce098d
Eric Andrade <vloxflox@gmail.com>
2019-10-20 21:26:12 -0300

    adicionando breeding pool

--- include/population.h
+++ include/population.h
@@ -12,16 +12,20 @@ using namespace std;
 class Population{
 public:
 	vector<Dna*> popArray;
-	vector<Dna*> partners;
+	vector<int>  partners;
 	int generations;
 	bool finished;
 	int perfectScore = CELLCOUNT*3;
 
 	void initializePop(int);
 	void calcFitness();
-	void generatePartners();
+	//void generatePartners();
+	void addToPartners(int);
+	void repopulatePartners();
+
 	void makeGeneration();
 	int  findFitest();
+	int  poolCountGenerator(int);
 
 	Population();
 
--- src/population.cpp
+++ src/population.cpp
@@ -4,7 +4,11 @@
 #include <iostream>
 #include <vector>
 
+#define PERFECTSCORE 48
+#define MAXADD 100
+
 using namespace std;
+
 
 Population::Population(){}
 
@@ -27,5 +31,23 @@ void Population::calcFitness(){
 
 	for(int i=0;i<popArray.size();i++){
 		cout<<popArray[i]->fitness<<endl;
+	}
+}
+
+void Population::addToPartners(int index){
+	int score = popArray.at(index)->fitness;
+	int poolCount = (MAXADD*score)/PERFECTSCORE; //regra de três para decidir quantos vão para a pool
+
+	for(int i=0;i<poolCount;i++){
+		partners.push_back(i);
+	}
+
+}
+
+void Population::repopulatePartners(){
+	partners.clear();	
+
+	for(int i=0;i<popArray.size();i++){
+		addToPartners(i);
 	}
 }
