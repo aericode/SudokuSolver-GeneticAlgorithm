31c3ef0c655c724e1242e60c3b692bd16e160363
Eric Andrade <vloxflox@gmail.com>
2019-11-18 20:52:35 -0300

    interface

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,40 @@
 using namespace std;
 
 int main(){
+
+	string examples[] ={"0001400000300200",
+						"2100032000041000",
+						"1034030020000001",
+						"0012010300004020",
+						"0230030000012040",
+						"4200100001200003",
+						"0003400001303020",
+						"0320000410000012",
+						"4000001002400021",
+						"0200002030402001",
+						"0001400220040023",
+						"4000000220140400",
+						"0210003201002001"};
+	int choice;
+	string tip;
+	do{
+		cout<<"escolha um dos exemplos para testar 0-12"<<endl;
+		cin>>choice;
+	}while(choice<0||choice>12);
+
+	tip = examples[choice];
+
+	float mut;
+
+	cout<<"escolha uma taxa de mutação entre 0 e 1"<<endl;
+	cout<<"(0.3 foi o usado nos testes)"<<endl;
+	cin>>mut;
+
+
+
 	srand(time(NULL));
 
-	Population population(2,0.4,"2100032000041000",100);
-	//Population population(3,0.3,"600120384008459072000006005000264030070080006940003000310000050089700000502000190",20);
+	Population population(2,mut,tip,100);
 
 	population.initializePop();
 
